d94befcdd5402311f09346b00f755e854786fabd
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-01-28 17:15:14 +0530

    Add files via upload

---
+++ string-challange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyCSharpMastery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            Console.WriteLine("Please enter your name and press enter: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(0, 5));
+            Console.Read();
+
+        }
+    }
+}
