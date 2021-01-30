6e0aa9944d2274d82ae68d1735f3482ead70627d
Yaseen <43935210+Yaseen549@users.noreply.github.com>
2021-01-30 14:08:52 +0530

    Add files via upload

---
+++ MyTestCase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyTestCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TestCase.CallCase call = new TestCase.CallCase();
+            call.Call();
+            MyHero();
+            Method(10);
+
+            Console.WriteLine(MyHero());
+            Console.WriteLine(Method(20));
+
+
+            Console.ReadKey();
+        }
+
+       static string MyHero()
+        {
+            string name = "I am your hero!";
+            Console.WriteLine("Am inside MyHero()");
+            return name;
+        }
+
+        static int Method(int a)
+        {
+            int b = a;
+            Console.WriteLine("Am Inside Method()");
+            return b;
+        }
+    }
+}
+
+namespace TestCase
+{
+    class CallCase
+    {
+        public void Call()
+        {
+            Console.WriteLine("Am Called Test Case!");
+        }
+    }
+}
